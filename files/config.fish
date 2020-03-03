96de56a3607b7b27f7950fada86499fc0ca7ee34
4leju <alejogithubarg@gmail.com>
2020-03-03 10:02:28 -0300

    v1

--- files/config.fish
+++ files/config.fish
@@ -23,11 +23,11 @@ end
 function fish
 clear
 echo -e "\033[92m V1"	
-echo "   .---,   "
-echo "  (    ~   4N? "
-echo "   \   /"
-echo "    ) (  "    
-echo "   /   \  "   
+echo -e "   .---,   "
+echo -e "  (    ~   4N? "
+echo -e "   \   /"
+echo -e "    ) (  "    
+echo -e "   /   \  \033[0mIngrese \033[91m'q' \033[0mpara Iniciar       "   
 end
 fish
 
@@ -40,14 +40,15 @@ proot -0 -w ~ $PREFIX/bin/bash
 end
 
 #ACTUALIZACION
-function 4A
+function updata
 git clone https://github.com/4leju/4N
 cd 4N/
-bash install.sh
+bash install
 end
 #Funcion Para Iniciar la terminal
-function IT
+function q
 fish
+echo -e "\033[92mACCESS GRANTED\033[0m"
 end
 
 #REPORTES
@@ -58,7 +59,7 @@ termux-open https://4name.simdif.com
 end 
 
 #Funcion Para los lideres
-function 4n 
+function 4NAME
 
 echo -e "\033[92m--4NAME--\033[0m"
 
@@ -70,7 +71,7 @@ echo -e "- \33[38;5;208mDana\033[0m "
 end
 
 #Funcion Para Entrar al almacenamiento
-function sd
+function sdcard
 echo "-sdcard"; cd /sdcard 
 end
 
@@ -90,17 +91,16 @@ end
 function ayuda
 echo "Ayuda Para 4N?"
 echo ""
-echo "COMANDO  DESCRIPCION"
+echo "COMANDO     DESCRIPCION"
 echo ""
-echo "4A       4name Actualizacion "
-echo "q        Inicio Terminal"
-echo "reporte  Pagina Web de Reportes "
-echo "rooted   Acceso de root para terminal"
-echo "sdcard   Acceder a almacenamiento interno"
-echo "4NAME    Creadores e lideres "
-echo "ddos     denegacion de servicio (ddos + ip) "
-echo "userr     busca en redes sociales usuarios"
-echo "guia     Guia De Termux"
+echo "updata      4name Actualizacion "
+echo "reporte     Pagina Web de Reportes "
+echo "rooted      Acceso de root para terminal"
+echo "sdcard      Acceder a almacenamiento interno"
+echo "4NAME       Creadores e lideres "
+echo "bash ddos   denegacion de servicio (ddos + ip) "
+echo "bash userr  Encuentre nombres de usuario en más de 75 redes sociales"
+echo "guia        Guia De Termux"
 end
 
 
